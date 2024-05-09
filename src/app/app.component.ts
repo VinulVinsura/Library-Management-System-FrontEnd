2e457090bf54ac95d17a7af9cfdc8adf2bc8959b
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-09 21:29:30 +0530

    Install bootstrap icon and Generate side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./comman/header/header.component";
+import { SideBarComponent } from './comman/side-bar/side-bar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent,SideBarComponent]
 })
 export class AppComponent {
   title = 'library-manage-app';
