c94dba6164d067b3e971ea45b86542544e4e4501
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-09 23:05:13 +0530

    Set Navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { BookListComponent } from './page/book-list/book-list.component';
 
 export const routes: Routes = [
-    
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"bookList",
+        component:BookListComponent
+    }
 ];
