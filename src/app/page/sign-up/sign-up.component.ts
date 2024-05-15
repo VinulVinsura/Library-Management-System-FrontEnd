3e7c68e58e3c92a9f52fa3bb28f0785453db4571
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-15 14:30:44 +0530

    implement country name and flags

--- src/app/page/sign-up/sign-up.component.ts
+++ src/app/page/sign-up/sign-up.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  private http;
+  public contrieList:any=[];
 
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.lodeCountries();
+  }
+
+  public lodeCountries() {
+    let api = 'https://restcountries.com/v3.1/all';
+    this.http.get(api).subscribe((data) => {
+      this.contrieList=data;
+      console.log(this.contrieList);
+    });
+  }
 }
