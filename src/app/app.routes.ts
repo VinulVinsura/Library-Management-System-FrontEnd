a0c6674cee61e6431205f7cde0e84b0547fa847e
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-15 10:29:32 +0530

    creat sign-Up Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { BookListComponent } from './page/book-list/book-list.component';
 import { LoginComponent } from './page/login/login.component';
+import { SignUpComponent } from './page/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path:"login",
         component:LoginComponent
+    },
+    {
+        path:"signUp",
+        component:SignUpComponent
     }
 ];
