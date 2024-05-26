017a61c11f204f12f1ad51c09a6c29fdbe900cd0
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-27 01:29:54 +0530

    find isExistsUserName validation

--- src/app/page/sign-up/sign-up.component.ts
+++ src/app/page/sign-up/sign-up.component.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StringDecoder } from 'node:string_decoder';
+import { text } from 'stream/consumers';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sign-up',
@@ -14,25 +16,24 @@ import { StringDecoder } from 'node:string_decoder';
 export class SignUpComponent implements OnInit {
   private http;
   public contrieList: any = [];
-  public selectCountry: any;
-  id="BR0";
-  i=0
+  public selectCountryCode: any;
+  public isExists:boolean=true;
+  id = 'BR0';
+  i = 0;
   borrower = {
-    borrower_Id:"dvdd",
-    username:null,
-    password:null,
-    firstName:null,
-    lastName:null,
-    email:null,
-    address:null,
-    country:null,
-    phoneNum:null
+    borrower_Id: 'dvdd',
+    username: null,
+    password: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    address: null,
+    country: null,
+    phoneNum: null,
   };
-  
 
   constructor(private httpClient: HttpClient) {
     this.http = httpClient;
-    
   }
 
   ngOnInit(): void {
@@ -41,7 +42,7 @@ export class SignUpComponent implements OnInit {
 
   public lodeCountries() {
     let api = 'https://restcountries.com/v3.1/all';
-    
+
     this.http.get(api).subscribe((data) => {
       this.contrieList = data;
       console.log(this.contrieList);
@@ -49,31 +50,52 @@ export class SignUpComponent implements OnInit {
   }
 
   public setCountry(country: any) {
-    this.selectCountry = country;
+    this.selectCountryCode = country.idd.root + country.idd.suffixes[0];
+    console.log(this.selectCountryCode);
     this.borrower.country = country.name.common;
   }
 
   public registerBorrower() {
-    this.borrower.borrower_Id=this.id + (++this.i)
-    console.log(this.borrower);
+    this.http
+      .get(
+        'http://localhost:8081/api/borrower/isExistsByUserName/' +
+          this.borrower.username,
+        { responseType:"text" }
+      )
+      .subscribe((data) => {
+        console.log(data)
+        
+        if (data=="false") {
+          this.borrower.borrower_Id = this.id + ++this.i;
+          let api = 'http://localhost:8081/api/borrower/addBorrower';
+          this.http.post(api, this.borrower).subscribe((data) => {
+            console.log(data);
+          });
+          Swal.fire({
+            title: "Borrower Saved!",
+            text: "You clicked the button!",
+            icon: "success"
+          });
 
-    let api = 'http://localhost:8081/api/borrower/addBorrower';
-    this.http.post(api, this.borrower).subscribe((data) => {
-      console.log(data);
-    });
-   
-    this.borrower = {
-      borrower_Id:"",
-      username:null,
-      password:null,
-      firstName:null,
-      lastName:null,
-      email:null,
-      address:null,
-      country:null,
-      phoneNum:null
-    };
-    
-    
+          this.borrower = {
+            borrower_Id: '',
+            username: null,
+            password: null,
+            firstName: null,
+            lastName: null,
+            email: null,
+            address: null,
+            country: null,
+            phoneNum: null,
+          };
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Can't Save Borrower..",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
+        }
+      });
   }
 }
