d3798d22443ce21f62510df6087b7977d93e9f7a
Vinul Vinsura <vinulviksura16@gmail.com>
2024-06-01 22:46:53 +0530

    creat search Book by Id

--- src/app/page/borrow-book/borrow-book.component.ts
+++ src/app/page/borrow-book/borrow-book.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "../../comman/header/header.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-borrow-book',
@@ -15,13 +16,43 @@ export class BorrowBookComponent {
   constructor(private http:HttpClient){}
 
   public userName:any;
-  public userDatils:any=""
+  public userDatils:any="";
+  public bookId:any="";
+  public selectBookList:any=[];
+  public book:any;
+  public confirmObj:any;
 
   searchUser(){
     console.log(this.userName);
     this.http.get(`http://localhost:8081/api/borrower/searchByUserName/${this.userName}`).subscribe((data)=>{
       console.log(data);
       this.userDatils=data
+    })
+  }
+
+  searchBook(){
+    console.log(this.bookId);
+    this.http.get(`http://localhost:8080/api/book/search/${this.bookId}`).subscribe((res)=>{
+      this.book=res
+      Swal.fire({
+        title: `Do you want to "${this.book.title}" Book ?`,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.selectBookList.push(res);
+          this.confirmObj=res;
+          console.log(this.selectBookList);
+          Swal.fire("Add to card !", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Not Added !", "", "info");
+        }
+      });
+      
+    
     })
   }
 
